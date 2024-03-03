79697562da2904ddd5a98e1b00dcd79cf63ca83f
Alina-prog-lang <143288938+Alina-prog-lang@users.noreply.github.com>
2024-03-03 23:28:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@ import logging
 import aiogram
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from aiogram.enums.dice_emoji import DiceEmoji
-
-TOKEN = "12345678"
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+TOKEN = " "
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
@@ -13,52 +12,41 @@ dp = Dispatcher(bot=bot)
 
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
-    user_id = message.from_user.id
     user_name = message.from_user.full_name
     await message.reply(f"Привет, {user_name}!")
 
 @dp.message(Command("place"))
 async def place(message: types.Message):
-    kb = [[types.KeyBoardButton(text="Онлайн")],
-          [types.KeyBoardButton(text="В офисе")]
-          ]
-    keyboard = types.ReplyKeyBoardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder = "Выберите формат"
-    )
-    await message("Онлайн/оффлайн", reply_markup=keyboard)
+    kb = types.ReplyKeyBoardMarkup(resize_keyboard=True)
+    kb.add(types.KeyBoardButton(text="Онлайн"))
+    kb.add(types.KeyBoardButton(text="В офисе"))
+    await message.answer("Онлайн/оффлайн", reply_markup=kb)
+
 
 @dp.message(Command("exp"))
 async def exp(message: types.Message):
-    kb = [[types.KeyBoardButton(text="Есть опыт работы")],
-          [types.KeyBoardButton(text="Без опыта работы")]
-          ]
-    keyboard = types.ReplyKeyBoardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder = "Опыт"
-    )
-    await message("Опыт", reply_markup=keyboard)
+    kb = types.ReplyBoardMarkup(resize_keyboard=True)
+    kb.add(types.KeyBoardButton(text="Есть опыт работы"))
+    kb.add(types.KeyBoardButton(text="Без опыта работы"))
+    await message.answer("Опыт", reply_markup=kb)
+
 
 @dp.message(Command("Activity"))
 async def activity(message: types.Message):
-    kb = [[types.KeyBoardButton(text="IT")],
-          [types.KeyBoardButton(text="Медицина")],
-          [types.KeyBoardButton(text="Преподавание")],
-          [types.KeyBoardButton(text="Дизайн")],
-          [types.KeyBoardButton(text="Экономика")],
-          [types.KeyBoardButton(text="Администратор")]
-          [types.KeyBoardButton(text="Право")]
-          ]
-    keyboard = types.ReplyKeyBoardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder = "Сфера деятельности"
-    )
-    await message("Сфера деятельности", reply_markup=keyboard)
+    kb = types.ReplyBoardMarkup(resize_keyboard=True)
+    kb.add(types.KeyBoardButton(text="IT"))
+    kb.add(types.KeyBoardButton(text="Медицина"))
+    kb.add(types.KeyBoardButton(text="Преподавание"))
+    kb.add(types.KeyBoardButton(text="Дизайн"))
+    kb.add(types.KeyBoardButton(types.KeyBoardButton(text="Экономика")))
+    await message.answer("Сфера деятельности", reply_markup=kb)
 async def main():
+    bot = Bot(token="TOKEN")
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
