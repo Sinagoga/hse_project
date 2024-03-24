59423e5def25fb90f9be92d8aaec6e87b632ece9
Alina-prog-lang <143288938+Alina-prog-lang@users.noreply.github.com>
2024-03-24 03:15:52 +0300

    Create total.py

---
+++ total.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, Router, types
+from handlers import result
+
+TOKEN = "6988515939:AAGuc60wFwC6nccacfiC1c3MNZTxFCe7h_Y"
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot=bot)
+router = Router()
+
+async def main():
+    dp.include_router(result.router)
+    try:
+        await dp.start_polling(bot)
+
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
