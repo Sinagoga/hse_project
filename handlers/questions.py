b0594e55abd87bf684929cbf54936ff4fd4e2fe9
Alina-prog-lang <143288938+Alina-prog-lang@users.noreply.github.com>
2024-04-02 16:26:08 +0300

    Update questions.py

--- handlers/questions.py
+++ handlers/questions.py
@@ -3,8 +3,8 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import emoji
 def choice() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Остаться на этой вакансии")
-    kb.button(emoji.emojize(":thumbs_up:"))
-    kb.button(emoji.emojize(":thumbs_down:"))
+    kb.add(KeyBoardButton(text="Остаться на этой вакансии"))
+    kb.add(KeyboardButton(text=emojize(":thumbs_up:")))
+    kb.add(KeyboardButton(text=emojize(":thumbs_down:")))
     kb.adjust(3)
     return kb.as_markup(resize_keyboard=True)
