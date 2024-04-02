c4a0b2a81acb24fb46e2b73035f0b9ac869115b2
Dima <lozhnii@yandex.ru>
2024-04-02 18:08:03 +0300

    done

--- handlers/questions.py
+++ handlers/questions.py
@@ -1,15 +1,16 @@
 from aiogram.types import ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.types.keyboard_button import KeyboardButton
 import emoji
 def choice() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
+    builder = InlineKeyboardBuilder()
     builder.row(
-        types.KeyboardButton(text=emojize(":thumbs_up:")),
-        types.KeyboardButton(text=emojize(":thumbs_down:"))
+        KeyboardButton(text=emoji.emojize(":thumbs_up:")),
+        KeyboardButton(text=emoji.emojize(":thumbs_down:"))
     )
     builder.row(
-        types.KeyBoardButton(text="Остаться на этой вакансии")
+        KeyboardButton(text="Остаться на этой вакансии")
     )
     return kb.as_markup(resize_keyboard=True)
     
