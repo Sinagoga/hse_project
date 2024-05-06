455fa3d3a7989ec4c5e6fe049a2e62acf03d1f2f
Tsanami <86549662+Tsanami@users.noreply.github.com>
2024-05-06 14:54:18 +0300

    slight changes

--- relevant_amazing_models.py
+++ relevant_amazing_models.py
@@ -1,38 +1,42 @@
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, dim_embedds, num_heads):
         super(MultiHeadAttention, self).__init__()
-        assert d_model % num_heads == 0
+        assert dim_embedds % num_heads == 0
         
-        self.d_model = d_model
+        self.dim_embedds = dim_embedds
         self.num_heads = num_heads
-        self.d_k = d_model // num_heads
+        self.d_k = dim_embedds // num_heads
         
-        self.W_q = nn.Linear(d_model, d_model)
-        self.W_k = nn.Linear(d_model, d_model)
-        self.W_v = nn.Linear(d_model, d_model)
-        self.W_o = nn.Linear(d_model, d_model)
-        
+        self.W_q = nn.Linear(dim_embedds, dim_embedds)
+        self.W_k = nn.Linear(dim_embedds, dim_embedds)
+        self.W_v = nn.Linear(dim_embedds, dim_embedds)
+        self.W_o = nn.Linear(dim_embedds, dim_embedds)
+        self.dropout = nn.Dropout(0)
+
     def scaled_dot_product_attention(self, Q, K, V, mask=None):
         attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
         if mask is not None:
             attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
-        attn_probs = torch.softmax(attn_scores, dim=-1)
-        output = torch.matmul(attn_probs, V)
+        attention = torch.softmax(attn_scores, dim=-1)
+        output = torch.matmul(self.dropout(attention), V)
         return output
-        
-    def split_heads(self, x):
-        batch_size, seq_length, d_model = x.size()
-        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
         
     def combine_heads(self, x):
         batch_size, _, seq_length, d_k = x.size()
-        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+        return x.permute(0, 2, 1, 3).contiguous().view(batch_size, seq_length, self.d_model)
         
     def forward(self, Q, K, V, mask=None):
-        Q = self.split_heads(self.W_q(Q))
-        K = self.split_heads(self.W_k(K))
-        V = self.split_heads(self.W_v(V))
-        
-        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
-        output = self.W_o(self.combine_heads(attn_output))
-        return output
+      batch_size, seq_length, _ = Q.size()
+
+      Q = self.W_q(Q)
+      K = self.W_k(K)
+      V = self.W_v(V)
+
+      Q = Q.view(batch_size, seq_length, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+      K = K.view(batch_size, seq_length, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+      V = V.view(batch_size, seq_length, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+
+
+      attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+      output = self.W_o(self.combine_heads(attn_output))
+      return output
